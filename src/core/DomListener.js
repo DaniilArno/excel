c9acd7925934c70caf5a5f352ccab97a8c647d21
DanielArno <arnautovdo93@gmail.com>
2020-05-21 12:45:56 +0300

    Finished base table logic

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,9 +1,9 @@
-import {capitalize} from '@core/utils';
+import {capitalize} from '@core/utils'
 
 export class DomListener {
   constructor($root, listeners = []) {
     if (!$root) {
-      throw new Error('root not provided to DomListener')
+      throw new Error(`No $root provided for DomListener!`)
     }
     this.$root = $root
     this.listeners = listeners
@@ -14,12 +14,16 @@ export class DomListener {
       const method = getMethodName(listener)
       if (!this[method]) {
         const name = this.name || ''
-        throw new Error(`Method ${method} is not implemented in ${name}`)
+        throw new Error(
+            `Method ${method} is not implemented in ${name} Component`
+        )
       }
       this[method] = this[method].bind(this)
+      // Тоже самое что и addEventListener
       this.$root.on(listener, this[method])
     })
   }
+
   removeDOMListeners() {
     this.listeners.forEach(listener => {
       const method = getMethodName(listener)
@@ -28,6 +32,9 @@ export class DomListener {
   }
 }
 
+// input => onInput
 function getMethodName(eventName) {
   return 'on' + capitalize(eventName)
 }
+
+
